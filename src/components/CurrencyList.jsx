58f6174c50121529605a0d9ded2eee17bcf399f1
unknown <Tonmoy01>
2022-10-03 06:44:13 +0600

    Prepare for deploy

---
+++ src/components/CurrencyList.jsx
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Coin from './Coin';
+import ReactPaginate from 'react-paginate';
+
+const CurrencyList = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const debounce = (fn, delay = 300) => {
+    let timerId;
+    return function (...args) {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        fn.apply(this, args);
+      }, delay);
+    };
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+  );
+
+  const pageCount = Math.ceil((filteredCoins?.length / 10).toFixed(0));
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <div className='coin-text'>Search a Currency</div>
+
+        <form>
+          <input
+            type='search'
+            placeholder='Search Currency'
+            className='coin-input'
+            onKeyUp={debounce(function (e) {
+              handleChange(e);
+            })}
+          />
+        </form>
+      </div>
+
+      {filteredCoins &&
+        filteredCoins
+          .slice((pageNumber - 1) * 10, (pageNumber - 1) * 10 + 10)
+          .map((coin) => {
+            return (
+              <Coin
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                market_cap={coin.market_cap}
+                price={coin.current_price}
+                priceChange={coin.market_cap_change_percentage_24h}
+                volume={coin.total_volume}
+              />
+            );
+          })}
+
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={'paginationBtns'}
+        previousLinkClassName={'previousBtn'}
+        nextLinkClassName={'nextBtn'}
+        disabledClassName={'paginationDisabled'}
+        activeClassName={'paginationActive'}
+      />
+    </div>
+  );
+};
+
+export default CurrencyList;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='navbar'>
+        Crypto Currency Tracker{' '}
+        <span className='version-text'>Version: 1.0.0</span>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
